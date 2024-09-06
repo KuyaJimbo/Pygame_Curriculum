20e45d1dc56063c33e7b90d16e48fcb95caa286f
James Ocampo <124105215+KuyaJimbo@users.noreply.github.com>
2024-09-06 09:13:14 -0400

    Easier Intro to python

---
+++ Calculator/l1_setup.py
@@ -0,0 +1,8 @@
+# Display a message with print()
+print("Hello, World!")
+
+# Create a variable to store the name -> change to input()
+name = "Coach"
+
+# Print the name
+print("Hello, " + name + "!")
